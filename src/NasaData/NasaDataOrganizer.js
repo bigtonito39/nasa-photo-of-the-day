18962485e8c80036697c2c089f25d84d72ee714c
bigtonito39 <bigtonito@hotmail.com>
2019-10-25 20:59:53 -0400

    Some styling added using styled components

--- src/NasaData/NasaDataOrganizer.js
+++ src/NasaData/NasaDataOrganizer.js
@@ -1,21 +1,53 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+padding: 6px 10px;
+  margin: 5px;
+  border: none;
+  border-radius: 12px;
+  color: white;
+  background: green;
+
+`;
+
+const ImageDiv = styled.div`
+
+background-image: url(http://t0.gstatic.com/images?q=tbn:ANd9GcQ9u48pu-6IB2FnnYl_H-15le_g8Dkt5d5RN-VWiWIl_-dyJdaa);
+background-repeat: no-repeat;
+
+  
+  `;
 
 const NasaDataOrganizer = props => {
-console.log(props)
+
+
+function changeNumber (){
+    
+   props.setNumber1(props.number1 + 1)
+
+};
+
+
+
 return (
-<body>
+
     <div className = "mainDiv">
 <header className = "Header-Sec">
+
  <h1>{`${props.title}`}</h1>
  <h5>{`  by ${props.copyright} on ${props.date} `} </h5>
-
+  
+ <Button onClick= {changeNumber} >Click for attendance</Button>
+ <h2>{`You are the visitor number: ${props.number1}`}</h2> 
 </header>
 <section className = "Image-sec">
-<div classname = "imageMainSec">
-<img src={props.url} alt="main image"/>
+<ImageDiv>
+<img src={props.url} alt="main"/>
 <p>{props.explanation}</p>
-</div> 
-  
+</ImageDiv> 
+
       
 </section>
     
@@ -23,8 +55,7 @@ return (
 <h2>Nasa Project!</h2>
     </footer>
     </div>
-    </body>
-
+ 
 );
 
 };
